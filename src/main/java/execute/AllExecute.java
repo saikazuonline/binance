652db93d9cd8f4d9c4e7639ce1b868cb1f115a08
kazuki saito <kazuki@kazuki.local>
2018-01-14 20:54:27 +0900

    created automation system

--- src/main/java/execute/AllExecute.java
+++ src/main/java/execute/AllExecute.java
@@ -1,6 +1,8 @@
 package execute;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import bean.TradeInfoBean;
 import currency.Calculation;
@@ -13,8 +15,40 @@ import currency.Impl.TradeImpl;
 import currency.Impl.NewCurrencySearchImpl;
 
 public class AllExecute {
-	public static void main(String args[]){
+	static int set_hour=1;//時間を設定
+	public static void main (String[] args){
+		Timer tim = new Timer();
+		tim.schedule(new Count(set_hour), 0,1000);
+	}
+}
 
+class Count extends TimerTask{
+	int count=0;
+	int set_hour;
+	public Count(int h){
+		set_hour=h*3600;//設定時間*3600秒（１時間）
+	}
+	
+	@Override
+	public void run() {//1秒 ごとに実行
+	if(count!=0){
+		count--;
+	}else{
+		new Get_and_Write().start();
+		count=set_hour;
+	}
+	//カウント
+	System.out.println("<count>"+count/3600+":"+count%3600/60+":"+count%60);
+	}
+}
+	
+class Get_and_Write extends Thread{//データを取得、ファイル書き出しClass
+	public void run(){
+	//try catch 等好きなように記述
+	autoBuy();
+}
+	
+	private void autoBuy(){
 		NewCurrencySearch newCurrencySearch = new NewCurrencySearchImpl();
 		Calculation calculation = new CalculationImpl();
 		Comparison comparison = new ComparisonImpl();
